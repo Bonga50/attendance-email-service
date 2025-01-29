49eeb2a155b1f9100b8f5885f96fb927bb80707f
Siyabonga <siyabonga4767@gmail.com>
2025-01-29 11:47:30 +0200

    added deno function

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-//import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
-// import { RRule } from "https://esm.sh/rrule";
-// import { CalendarEvent } from "https://esm.sh/angular-calendar";
-// import { addMonths, subMonths } from "https://esm.sh/date-fns@2.21.1";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.7.1";
+import { RRule } from "https://esm.sh/rrule";
+import { CalendarEvent } from "https://esm.sh/angular-calendar";
+import { addMonths, subMonths } from "https://esm.sh/date-fns@2.21.1";
 
 const handler = async (req: Request): Promise<Response> => {
-  try {
-    console.log("hello");
-    return new Response("Processing complete", { status: 200 });
-  } catch (error) {
-    console.error("Error:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
-};
+    try {
+      console.log("hello");
+      return new Response("Processing complete", { status: 200 });
+    } catch (error) {
+      console.error("Error:", error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
+  };
 
-export default handler;
+Deno.serve(handler);
